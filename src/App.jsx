339b3c90955d94f3217d51bb5c4dae4701ea5fd7
AndresMGdev <cmendoza022002@gmail.com>
2023-06-11 17:24:14 -0500

    new project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Encrypt from './components/Encrypt'
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Encrypt />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '/logo.png'
+
+function Header() {
+  return (
+    <header>
+        <img className='logo' src={logo} alt="Logo Alura" />
+    </header>
+  )
+}
+
+export default Header
