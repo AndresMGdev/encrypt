12d3744653c87f98b22f34675b9695f6eb1c8fab
AndresMGdev <cmendoza022002@gmail.com>
2024-03-12 02:19:17 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/encriptador-de-texto",
+  site: "https://andresmgdev.github.io",
+  base: "/encrypt",
   server: {
     host: true,
     port: 8000,
