18d2edfa06560dd01471dbb9deee3ef8f79b709e
AndresMGdev <cmendoza022002@gmail.com>
2024-03-12 01:35:49 -0500

    changes in css and images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import logo from '/logo.png'
+import logo from '/me.png'
 
 function Header() {
   return (
     <header>
-        <img className='logo' src={logo} alt="Logo Alura" />
+        <img className='logo' src={logo} alt="Logo me" />
+        <div className="title-head">
+          <h1>Text Encrypted</h1>
+        </div>
     </header>
   )
 }
